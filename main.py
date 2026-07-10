d664e43b41aa8a7b5080bca73881a431ec12836f
minjunrrrr <108181298+minjunrrrr@users.noreply.github.com>
2026-07-10 16:18:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,18 @@ st.code("a=3")
 
 st.text("안녕~~ 남동고등학교 여러분,, 첫 페이지를 만드셨습니다.")
 
+
+df = pd.read_csv('등산경로'.csv, encoding= 'utf-8')
+df_lation = df[['위도','경도']]
+df_lation = df_lation.rename(columns={'위도':'lat','경도':lon'})
+#st.map(df_lation)
+
 # 3. 지도 생성 및 마커 표시(지도 시각화 단계)
 m = folium.Map(
     location=[37.40583317,126.7214872],
     zoom_start=12
 )
+
 folium.Marker(
   location= [37.404160, 126.719249],
   popup= "ㅁ ㅁ ㅁ",
@@ -37,7 +44,24 @@ folium.Marker(
 
 for i in range(len(df)):
     folium.Marker(
-        location =
+        location = [df.iloc[i]['위도'], df.iloc[i]['경도']],
+        popup=f'div style="width:200px"> <strong>{df.iloc[i]['위치명']}</strong></div>',
+        tooltip="클릭해보세요"
+        icon = folium.Icon(color='green', icon= 'info-sign')
+        ).add_to(m)
+        
 # 4. 화면 출력
 st_folium(m, width=700, height=500)
 '''
+
+# 4. 화면 출력
+col1 , col1 = st.colums([3.1])
+
+with col1:
+    st_folium(m, width=700, height=500)
+
+with col2:
+    st.subheader("정보") # 코스정보
+    st.info("길이 미끄럽습니다. 주의하세요.")
+    st.martric(lable="소요시간", value="10분") # 소요시간, 정보 코스별로 넣기
+    st.write("주의사항 : 등산화를 착용하세요. ")
