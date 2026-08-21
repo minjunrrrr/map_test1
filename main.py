44d6f080fee2e8a7be86ff8d017a333fd289ba29
minjunrrrr <108181298+minjunrrrr@users.noreply.github.com>
2026-08-21 14:45:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ with col1:
 with col2:
     st.subheader("정보") # 코스정보
     st.info("길이 미끄럽습니다. 주의하세요.")
-    st.martric(label="소요시간", value="10분") # 소요시간, 정보 코스별로 넣기
+    st.metric(label="소요시간", value="10분") # 소요시간, 정보 코스별로 넣기
     st.write("주의사항 : 등산화를 착용하세요. ")
